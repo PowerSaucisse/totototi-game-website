53f7e3191f0828e7ad850780874fc0b6e3baac4f
Estéban Ristich <esteban.ristich@protonmail.com>
2022-01-24 13:29:14 +0100

    Add navbar hover scale animation

--- components/NaviguationBar.vue
+++ components/NaviguationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav>
-      Cheeeh!
+      <h1>Cheeeh!</h1>
     </nav>
   </header>
 </template>
@@ -18,19 +18,31 @@ export default defineComponent({
 
 <style scoped>
   header{
-    border-bottom: 1px solid #E2E8F0;
     background-color: rgba(51, 48, 46, 1.000);
     border-bottom: solid 2px var(--color-primary);
   }
 
   nav {
+    overflow: hidden;
+  }
+
+  nav h1 {
+    margin: 0;
+    padding: 0;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-end;
     padding: 1rem 1.5rem;
     font-family: 'giant-mouse';
     font-size: 50px;
     color: rgba(235, 182, 126, 1.000);
     font-weight: 20px;
+    text-shadow: 3px -3px 0 rgb(180, 131, 78);
+    user-select: none;
+    transition: 0.2s ease-in;
+  }
+
+  h1:hover {
+    transform: scale(1.1);
   }
 </style>
